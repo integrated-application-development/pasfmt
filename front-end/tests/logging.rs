6e68c78548bdd6711da17bd324beeb9e28c5b342
Zane Duffield <zduffield@integradev.com.au>
2025-08-21 14:36:23 +1000

    Add simple integration test to cover trace logging

---
+++ front-end/tests/logging.rs
@@ -0,0 +1,41 @@
+use assert_fs::{prelude::*, TempDir};
+use predicates::prelude::*;
+
+use crate::utils::*;
+
+/// The trace logging done by the [pasfmt_core::rules::optimising_line_formatter]
+/// is quite complex, and has the potential to crash due to a bug.
+///
+/// While not covering all cases, this test will ensure that the basic
+/// functionality of the logging works without crashing.
+#[test]
+fn trace_logging_does_not_crash() -> TestResult {
+    let tmp = TempDir::new()?;
+
+    let file = tmp.child("a.pas");
+    file.write_str(
+        "
+if True then
+  Foo;
+
+A :=
+    procedure
+    begin
+      var B :=
+          procedure
+          begin
+            Foo;
+          end;
+    end;
+",
+    )?;
+
+    pasfmt()?
+        .arg("--log-level=TRACE")
+        .arg(&*file)
+        .assert()
+        .success()
+        .stderr(predicate::str::contains("TRACE"));
+
+    Ok(())
+}
